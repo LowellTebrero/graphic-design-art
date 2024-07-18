eb15140f07ccf3357fa79e4530a30b5b9d1391ed
LowellTebrero <lowelltebrero@gmail.com>
2024-07-18 16:28:50 +0800

    first commit

--- index.js
+++ index.js
@@ -56,30 +56,3 @@ document.addEventListener('DOMContentLoaded', handleSidebar);
 // Call the function initially
 handleSidebar();
 
-
-// function handleResize() {
-//     const navList = document.getElementById('navList');
-//     if (window.innerWidth <= 640) {
-//         if (navList) {
-//             navList.remove();
-//         }
-//     } else {
-//         if (!document.getElementById('navList')) {
-//             const navDiv = document.querySelector('.nav');
-//             const newNavList = document.createElement('ul');
-//             newNavList.className = 'flex gap-x-8 lg:gap-x-12 xl:gap-x-16 hidden sm:block';
-//             newNavList.id = 'navList';
-//             newNavList.innerHTML = `
-//                 <li><button type="button" class="px-2 rounded-sm font-light text-base collection" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0">Home</button></li>
-//                 <li><button type="button" class="px-2 rounded-sm font-light text-base collection" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1">Product</button></li>
-//                 <li><button type="button" class="px-2 rounded-sm font-light text-base collection" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2">About</button></li>
-//                 <li><button type="button" class="px-2 rounded-sm font-light text-base collection" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3">Contact</button></li>
-//                 <li><button type="button" class="px-2 rounded-sm font-light text-base collection" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4">IDK</button></li>
-//             `;
-//             navDiv.appendChild(newNavList);
-//         }
-//     }
-// }
-
-// window.addEventListener('resize', handleResize);
-// document.addEventListener('DOMContentLoaded', handleResize);
